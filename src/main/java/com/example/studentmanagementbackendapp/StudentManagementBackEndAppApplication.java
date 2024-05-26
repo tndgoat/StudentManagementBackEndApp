3eabb663a7b346d5f207a157f0d914655843c313
tndgoat <tungnd.goat@gmail.com>
2024-05-26 22:29:02 +0700

    init the project

---
+++ src/main/java/com/example/studentmanagementbackendapp/StudentManagementBackEndAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagementbackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementBackEndAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementBackEndAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentmanagementbackendapp/StudentManagementBackEndAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagementbackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementBackEndAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
