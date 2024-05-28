5cd6a41f4921a0e04f07e2b10eb246cba114fd5e
tndgoat <tungnd.goat@gmail.com>
2024-05-28 20:14:32 +0700

    update be

--- src/main/java/com/example/studentmanagementbackendapp/controller/StudentController.java
+++ src/main/java/com/example/studentmanagementbackendapp/controller/StudentController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/students")
 @CrossOrigin
+@RequestMapping("/api/students")
 public class StudentController {
     @Autowired
     private StudentService studentService;
